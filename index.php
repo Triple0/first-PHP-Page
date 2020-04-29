f723f8b864662f940967ca5473ae37dd4d234d88
Triple0 <lekan.olapeju@yahoo.com>
2020-04-29 07:03:50 +0100

    First PHP Exercise

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP Page</title>
+</head>
+<body>
+<h1>First PHP Page </h1>
+<?php 
+    echo 'Hello World!';
+?>
+    
+</body>
+</html>
